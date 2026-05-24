5c937d92711631d439599ddefd7bb6f2bb5eb0d1
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-25 00:29:52 +0600

    ohh forgot to push the work flows now guy's it's time to sleep see ya tomorrow

---
+++ maths.java
@@ -0,0 +1,14 @@
+public class maths {
+    public static void main(String[] args) {
+        //maths
+        //5, 6
+        //random
+        System.out.println(Math.max(5,6));
+        System.out.println(Math.min(5,6));
+
+        // Math random prints random values
+        System.out.println((int)(Math.random()*100));
+        // Here we multiply it too 100 cause the default limit of the random values are too short.
+    }
+    
+}
