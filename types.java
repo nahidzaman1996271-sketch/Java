b7632245abb5a7485728129d5e9fc71831b4107b
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-24 14:37:44 +0600

    learning slowly

---
+++ types.java
@@ -0,0 +1,19 @@
+public class Main{
+
+    public static void main(String[] args) {
+        // byte - 1 [-128 to 127]
+        //short - 2
+        //int - 4
+        //long - 8
+        //float - 4
+        //double - 8
+        //char - 2
+        //boolean - 1 true/false
+
+        byte age = 40;
+        long phone = 19767365337L;
+        float pi = 3.14F;
+        char letter = 'a';
+        boolean isAdult = true;
+    }
+}
