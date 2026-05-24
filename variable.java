abc274249465f926bd92f4dacb202ec41977e2d0
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-24 14:30:22 +0600

    slowly learning things

---
+++ variable.java
@@ -0,0 +1,11 @@
+public class variable {
+    public static void main(String[] args) {
+        // variables
+        String name = "Nahid";
+        int age = 30;
+        String neighbour = "Ibn";
+
+        String friend = neighbour;
+        System.out.printf("%s %d %s%n", name, age, neighbour);
+    }
+}
