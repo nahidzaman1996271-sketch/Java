d1ac8d643a2d5974eb1496380e2e9ae2d49ada49
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-24 23:35:10 +0600

    and here end's the operator

---
+++ operators.java
@@ -0,0 +1,18 @@
+public class operators {
+    
+    public static void main(String[] args) {
+        //operators
+        double a = 1;
+        double b = 2;
+        double sum = a + b;
+        double diff = a - b;
+        double mul = a * b;
+        double div = a / b;
+        double mod = a % b;
+        System.out.println(sum);
+        System.out.println(div);
+        System.out.println(mul);
+        System.out.println(diff);
+        System.out.println(mod);
+    }
+}
