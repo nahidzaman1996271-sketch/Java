263284ae2c1b3b8a77d78305f88edd7492764dcd
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-24 15:07:47 +0600

    there are lot more to go

---
+++ strings.java
@@ -0,0 +1,11 @@
+public class strings {
+
+    public static void main(String[] args) {
+        // strings
+        // concatenate
+        String name1 = "Nahid";
+        String name2 = "Ibn";
+        String name3 = name1 + " and " + name2;
+        System.out.println(name3);
+    }
+}
