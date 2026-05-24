a47b421cedbfe57a745cbce50576c9670d062a4d
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-24 23:00:07 +0600

    and here we learns how to do 2d arrays

---
+++ arrays_2d.java
@@ -0,0 +1,10 @@
+public class arrays_2d{
+
+    public static void main(String[] args){
+       int[] marks = {97, 98, 95};
+
+       int[][] finalMarks = {{97,98,96},{95,95,96}};
+       System.out.println(finalMarks[1][1]);
+    }
+    
+}
