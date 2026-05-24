e0f58918a266d329fd87fa41752f790bdc6c01d8
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-24 15:20:55 +0600

    and here end's this part of string time's to learn substring

--- strings2.java
+++ strings2.java
@@ -1,3 +1,11 @@
 public class strings2 {
     
+    public static void main(String[] args) {
+        //strings
+        //charAt
+        String name = "Mahmuda";
+        String name2 = name.replace('a','b');
+        System.out.println(name); //older one
+        System.out.println(name2); //new one after replacing
+    }
 }
