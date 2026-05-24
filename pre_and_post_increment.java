b4feeef3fb5edfe91b9189b33e9cfa1b827b6c92
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-25 00:08:35 +0600

    and here it is and we done this part also and made this far

--- pre_and_post_increment.java
+++ pre_and_post_increment.java
@@ -6,5 +6,7 @@ public class pre_and_post_increment{
     int numb = 1;
     System.out.println(++numb); //this is the preincrement part
     System.out.println(numb);  //and this is after the preincrement
+    System.out.println(numb--); //this will decrement one value to the next steps
+    System.out.println(numb); //and here the value will be decrements and will become 1
     }
 }
