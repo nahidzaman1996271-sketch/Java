ea396b62c24f5972031b03c7f5ffa98dbee2582f
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-24 14:55:53 +0600

    and here we learn what is primitive types

---
+++ non_primitive_types.java
@@ -0,0 +1,8 @@
+public class non_primitive_types {
+    
+    public static void main(String[] args) {
+        //Non-primitive types
+        String name = "Nahid"; 
+        System.out.println(name.length());
+    }
+}
