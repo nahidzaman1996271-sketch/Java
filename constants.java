dcfc9c033705e5634fbb829ac7dbbddc5555f8ff
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-24 23:23:43 +0600

    and here it is

---
+++ constants.java
@@ -0,0 +1,13 @@
+public class constants {
+    
+    public static void main(String[] args) {
+        //constants
+        int age = 30;
+        age = 31;
+        age = 32;
+        age = 35;
+
+        final float PI = 3.14F;
+        PI = 1.1F; //this will not work cause we defined final in the earlier line
+    }
+}
