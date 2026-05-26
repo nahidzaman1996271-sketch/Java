8f100b631386fa6e538cff5b5707b6e2e172b802
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-26 13:06:05 +0600

    just learnt comparison's in Java and everything in Java is so critical and need's lot more practice to memorize it  now this is for noon see ya guy's after a while so until then take care and pray for me goodbye

---
+++ conditionals_statements.java
@@ -0,0 +1,22 @@
+public class conditionals_statements{
+
+    public static void main(String[] args){
+        //conditional statements...
+        boolean isSunUp = false;
+        if(isSunUp == true){
+            System.out.println("day");
+        }
+        else{
+            System.out.println("night");
+        }
+
+        System.out.println("Below one is for vote just testing another one");
+        
+        int age = 30;
+        if(age > 18)
+            System.out.println("Can vote");
+        else
+            System.out.println("Can't vote");
+        
+    }
+}
