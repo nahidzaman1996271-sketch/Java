45f3aa58341413b3f94527206978de7be9b1ba80
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-26 12:47:48 +0600

    Ngl taking input in Java is so cooked man this is too difficult

--- input.java
+++ input.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class input {
     
     public static void main(String[] args) {
-        System.out.println("Nahid is good boy!!");
+        //How to take input..
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input your Name: ");
+        String name = sc.nextLine();
+        System.out.println(name);
     }
 }
