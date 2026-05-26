56b675e29f4b63af4424e3d63c4c4884ccf88849
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-26 12:55:59 +0600

    gotta learn this

---
+++ comparison.java
@@ -0,0 +1,10 @@
+public class comparison{
+
+    public static void main(Strnig[] args){
+        // Comparison operators
+        // a == b -> equal operator
+        // a != b -> so a not equal to b
+        // a < b -> a is less than b
+        // a <= b -> b is greater or equal to a
+    }
+}
