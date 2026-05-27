07892f95d8a8561a16f89a5efe2a7fc119bd6374
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-27 21:48:28 +0600

    and here end's the for loops parts

---
+++ for_loops.java
@@ -0,0 +1,18 @@
+public class for_loops {
+    
+    public static void main(String[] args) {
+        for(int i=1; i<=100; i++){
+            System.out.println(i);
+        }
+        
+    System.out.println("Now we are gonna print them in reverse order: ");
+        for(int i=100; i>=1; i--){
+            System.out.println(i);
+        }
+
+    }
+}
+
+// This is for loops...
+
+// There are three parts in for loops 1.initialization 2.condition 3.increment...
