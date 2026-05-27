1c41af90454089f3e80dfd9fe5c238c39af41c7f
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-27 21:41:27 +0600

    just this is i will create each loops part by part

---
+++ loops.java
@@ -0,0 +1,12 @@
+public class loops {
+    
+    public static void main(String[] args) {
+        //LOOPS
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        //.................. and so other numbers to prevent this we use loops
+        //There are three loops
+        //For loops, while, and do loops
+    }
+}
