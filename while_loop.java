ef47d4f8d74e7610328b894a84e97d06e8e426a5
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-27 21:56:16 +0600

    here end's the while part now we are moving to the do while loops

---
+++ while_loop.java
@@ -0,0 +1,17 @@
+public class while_loop {
+    
+    public static void main(String[] args) {
+
+        int i = 1;
+        while(i <= 100){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Now we are gonna print them in reverse order: ");
+        int j = 100;
+        while(j >= 1){
+            System.out.println(j);
+            j--;
+        }
+    }
+}
