087ff2895b1184005796c84073883112df7182d8
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-27 20:45:08 +0600

    learnt another thing that no need to write whole the things

---
+++ logical_operators_using_bool.java
@@ -0,0 +1,10 @@
+public class logical_operators_using_bool {
+    
+    public static void main(String[] args){
+        boolean isAdult = false;
+        if(!isAdult)
+            System.out.println("Yes he is adult");
+        else
+            System.out.println("not adult");
+    }
+}
