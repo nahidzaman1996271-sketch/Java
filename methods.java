8a15fdce3320af7d99d7c262fc2a009499046f89
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-27 22:44:50 +0600

    it is just function

---
+++ methods.java
@@ -0,0 +1,12 @@
+public class methods {
+    public static void printJava(){
+        System.out.println("Hello Nahid");
+    }
+    public static void main(String[] args){
+        //METHODS
+        printJava();
+        printJava();
+        printJava();
+    }
+}
+
