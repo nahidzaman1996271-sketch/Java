80a38b6ad535b8704a119767a2d1ae69c8002162
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-27 20:39:34 +0600

    learning && and || operators in Java

--- logical_operators.java
+++ logical_operators.java
@@ -1,7 +1,21 @@
+
+import javax.sound.sampled.SourceDataLine;
+
 public class logical_operators{
     
     public static void main(String[] args){
         //logical operators
         // && operators
+        int a = 30;
+        int b = 60;
+
+        if(a<50 && b<50){
+            System.out.println("both of them less than 50");
+        }
+
+        // below is or operator
+        if(a<50 || b<50){
+            System.out.println("Yes one of them are 50");
+        }
     }
 }
